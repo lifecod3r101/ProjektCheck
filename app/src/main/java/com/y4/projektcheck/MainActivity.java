88c53a9447cc1cf69eb26bfa95260c1f7bad65bf
lifecod3r101 <speedymax32@gmail.com>
2022-03-25 22:07:43 +0300

    Initial commit

---
+++ app/src/main/java/com/y4/projektcheck/MainActivity.java
@@ -0,0 +1,14 @@
+package com.y4.projektcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
